21289c9c90114a8a00f6a86ff0face34493a2fdc
Juanki0396 <juanki0396@gmail.com>
2022-03-10 23:05:11 +0100

    Cleaning Checker and InputValidator classes

--- src/game.py
+++ src/game.py
@@ -1,52 +1,66 @@
 import random
 
+from typing import List
+
 
 class Checker():
 
-    def __init__(self, attempt, solution):
+    @staticmethod
+    def delete_matchs(word: str, position_map: str) -> str:
+        """Substitute the letters in word with a '-' if they coincide
+        in position with a 1 in postion map
 
-        self.attempt = list(attempt)
-        self.solution = list(solution)
-        print(f'SOLUTION: {solution}')
-
-    def check_correct_letters(self) -> list:
-        """ Search from the input solution the letters that appear in the final answer, and are placed correctly.
+        Args:
+            word (str): word to process
+            position_map (str): encode letters to delete in 
 
         Returns:
-            list: generates a preliminary map of the results of the input attempt:
-                  1 - Corresponds to a correct letter, placed in the right place
-                  '-' - Corresponds to a letter that either is part of the solution, or it is wrong
+            str: _description_
         """
-        output = []
-        solution = self.solution.copy()
-        attempt = self.attempt.copy()
-        for index, letter in enumerate(self.attempt):
-            if letter == self.solution[index]:
-                output.append('1')
-                solution[index] = '-'
-                attempt[index] = '-'
-            else:
-                output.append('-')
+        if not len(word) == len(position_map):
+            raise RuntimeError("Parameters must have the same lenght")
+
+        matched_word = [letter if key != "1" else "-" for letter,
+                        key in zip(word, position_map)]
+        return "".join(matched_word)
+
+    def __init__(self, attempt: str, solution: str):
+
         self.attempt = attempt
         self.solution = solution
-        self.position_map = output
 
-        return output
+    def check_correct_letters(self) -> str:
+        """ Search from the solution the letters that appear in the final answer, and are placed correctly.
 
-    def check_missplaced_letters(self) -> list:
+        Returns:
+            str: generates a preliminary map of the results of the input attempt:
+                  '1' - Corresponds to a correct letter, placed in the right place
+                  '-' - Corresponds to a letter that either is part of the solution, or it is wrong
+        """
+        position_map = ""
+
+        for index, letter in enumerate(self.attempt):
+            if letter == self.solution[index]:
+                position_map += "1"
+            else:
+                position_map += "-"
+
+        return position_map
+
+    def check_missplaced_letters(self, preliminary_map: str) -> str:
         """Search for the letters that are present in the final answer, but are missplaced.
 
         Returns:
-            list: generates a final map of the results of the input attempt
+            str: generates a final map of the results of the input attempt
                   2 - Corresponds to a letter that is present in the final answer, but it's missplaced. 
                   1 - Corresponds to a correct letter, placed in the right place
                   0 - Corresponds to a wrong letter
         """
+        attempt = Checker.delete_matchs(self.attempt, preliminary_map)
+        output = list(preliminary_map)
+        solution = list(Checker.delete_matchs(self.solution, preliminary_map))
 
-        output = self.position_map.copy()
-        solution = self.solution.copy()
-
-        for index, letter in enumerate(self.attempt):
+        for index, letter in enumerate(attempt):
             if letter == '-':
                 continue
             if letter in solution:
@@ -55,39 +69,51 @@ class Checker():
             else:
                 output[index] = '0'
 
-        self.position_map = output
-        return output
+        return "".join(output)
+
+    def make_checks(self) -> str:
+        """Runs the checking cycle
+
+        Returns:
+            str: position_map 
+        """
+        position_map = self.check_correct_letters()
+        position_map = self.check_missplaced_letters(position_map)
+        return position_map
+
+# TODO Write docstrings
 
 
-class Inputer():
+class InputValidator():
 
     def __init__(self, word_list: list):
         self.word_list = word_list
-        keep_asking = True
-        while keep_asking:
-            self.word = input('Enter your word: ').lower()
-            if not self.check_lenght():
-                print('The word must have 5 letters')
-                continue
-            if self.check_validity():
-                break
-            else:
-                print('Word is not in the allowed solutions')
 
     def get_word(self):
         return self.word
 
     def check_lenght(self):
-        if len(self.word) == 5:
-            return True
-        else:
-            return False
+        return len(self.word) == 5
 
     def check_validity(self):
-        if self.word in self.word_list:
-            return True
-        else:
-            return False
+        return self.word in self.word_list
+
+    def request_word(self):
+        self.word = input('Enter your word: ').lower()
+
+    def make_checks(self) -> bool:
+        keep_asking = not self.check_lenght() and not self.check_validity()
+        if keep_asking:
+            print("Word must be in the valid words list and have 5 letters.")
+        return keep_asking
+
+    def run_validation_cycle(self):
+        keep_asking = True
+        while keep_asking:
+            self.request_word()
+            keep_asking = self.make_checks()
+
+# TODO Restructure the code
 
 
 class Wordle():
@@ -106,11 +132,10 @@ class Wordle():
         life = 1
         win = False
         while life < 7:
-            inputer = Inputer(self.word_list)
-            word = inputer.get_word()
+            validator = InputValidator(self.word_list)
+            word = validator.get_word()
             checker = Checker(word, solution)
-            checker.check_correct_letters()
-            position_map = checker.check_missplaced_letters()
+            position_map = checker.make_checks()
             self.draw(word, position_map, life)
             if self.is_game_won(position_map):
                 win = True
@@ -131,7 +156,7 @@ class Wordle():
         print(formated_position_map)
 
     def is_game_won(self, position_map):
-        if position_map == ['1']*5:
+        if position_map == '1'*5:
             return True
         else:
             return False
